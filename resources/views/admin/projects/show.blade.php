aa94d3be75f24822f000ba523a58af6af2d329d2
juri931 <impa.mat@hotmail.it>
2024-05-28 17:04:48 +0200

    "Refactored project show page layout, rearranged and grouped project details and technologies list"

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,19 +3,24 @@
 @section('content')
     <h2>{{ $project->name }}</h2>
 
-    <small class="mt-4 d-block">Categoria: {{ $project->category }}</small>
+    <div class="top-container d-flex justify-content-around">
+        <div class="d-flex flex-column">
+            <small class="d-block">Categoria: {{ $project->category }}</small>
+            <small class="d-block">Creato: {{ $project->created }}</small>
+            <small class="d-block">Ultimo aggiornamento: {{ $project->updated_at }}</small>
+        </div>
 
-    <small class="d-block">Creato: {{ $project->created }}</small>
-    <small class="d-block">Ultimo aggiornamento: {{ $project->updated_at }}</small>
-    <small>
-        <ul>
-            Tecnologie:
-            @foreach ($project->technologies as $technology)
-                <li>{{ $technology->name }}</li>
-            @endforeach
-
-        </ul>
-    </small>
+        <div class="d-flex">
+            <small>
+                <ul>
+                    Tecnologie utilizzate:
+                    @foreach ($project->technologies as $technology)
+                        <li class="list-unstyled">{{ $technology->name }}</li>
+                    @endforeach
+                </ul>
+            </small>
+        </div>
+    </div>
 
 
     <img src="{{ $project->image }}" alt="{{ $project->name }}.jpg">
