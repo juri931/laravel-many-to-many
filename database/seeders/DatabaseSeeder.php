432ffbb2e38fcab1f4c8421aed9cd04a397e0203
juri931 <impa.mat@hotmail.it>
2024-05-28 16:07:27 +0200

    "Added ProjectTechnologyTableSeeder to DatabaseSeeder"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             TechnologyTableSeeder::class,
             TypeTableSeeder::class,
             ProjectTableSeeder::class,
+            ProjectTechnologyTableSeeder::class,
         ]);
     }
 }
