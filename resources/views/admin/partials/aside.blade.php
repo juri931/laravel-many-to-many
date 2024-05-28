ca29035ec1fd2bef98581f64f13278c529471b71
juri931 <impa.mat@hotmail.it>
2024-05-28 15:49:23 +0200

    scaffolding

---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,21 @@
+<aside class="bg-dark navbar-primary text-white">
+    <nav>
+        <ul>
+            <li>
+                <a href="{{ route('admin.projects.index') }}"><i class="fa-solid fa-terminal"></i>Progetti</a>
+            </li>
+            <li>
+                <a href="{{ route('admin.projects.create') }}"><i class="fa-solid fa-circle-plus"></i>Nuovo Progetto</a>
+            </li>
+            <li>
+                <a href="{{ route('admin.technologies.index') }}"><i class="fa-solid fa-list-check"></i>Tecnologie</a>
+            </li>
+            <li>
+                <a href="{{ route('admin.types.index') }}"><i class="fa-solid fa-tags"></i></i>Types</a>
+            </li>
+            <li>
+                <a href="{{ route('admin.type_projects') }}"><i class="fa-solid fa-network-wired"></i>Tipologia/Progetti</a>
+            </li>
+        </ul>
+    </nav>
+</aside>
