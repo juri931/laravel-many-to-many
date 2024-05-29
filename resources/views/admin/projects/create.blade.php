93c8b792b10f7a9f6b0637d8b2055a2941cb1d3b
juri931 <impa.mat@hotmail.it>
2024-05-29 16:29:09 +0200

    "Added form fields and layout to admin/projects/create.blade.php"

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -2,7 +2,28 @@
 
 @section('content')
 
-<h1>Form creazione progetto</h1>
-<form action="{{route('admin.projects.store')}}" method="POST">
+    <h1>Form creazione progetto</h1>
+
+    <form action="{{route('admin.projects.store')}}" method="POST">
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="name">Nome</label>
+                <input type="text" class="form-control" name="name">
+            </div>
+
+            <div class="form-group col-md-6">
+                <label for="category">Categoria</label>
+                <input type="text" class="form-control" name="category">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="description">Descrizione</label>
+            <textarea class="form-control dropdown dropdown-item-text" name="description"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary my-2">Invia</button>
+    </form>
+
 
 @endsection
